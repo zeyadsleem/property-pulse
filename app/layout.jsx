908d1c13a638269768f181b9b85d99be72cdd1ba
zeyadsleem <zeyadsleem1@gmail.com>
2024-11-09 23:52:10 +0200

    Add Homepage Components (Hero, InfoBoxes, InfoBox, Footer)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
       >
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
